8b168785e4c82a2203483ca2feeec7bbc43308c3
Huy Pham <benjaminhuypham@gmail.com>
2019-08-28 06:24:27 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,7 +8,7 @@ end
 def find_max_value(array)
     # Add your solution here
     new_array = array.sort 
-    puts new_array.last
+    p new_array.last
 end 
 
 find_max_value([1,2,1,3,4,3,5,4,3,2,1])
