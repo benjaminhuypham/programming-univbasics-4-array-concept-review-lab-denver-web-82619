790467402646225bb396a464b9180677247a8f2d
Huy Pham <benjaminhuypham@gmail.com>
2019-08-28 06:23:54 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -8,7 +8,7 @@ end
 def find_max_value(array)
     # Add your solution here
     new_array = array.sort 
-    puts array.last
+    puts new_array.last
 end 
 
 find_max_value([1,2,1,3,4,3,5,4,3,2,1])
@@ -16,8 +16,8 @@ find_max_value([1,2,1,3,4,3,5,4,3,2,1])
 
 def find_min_value(array)
   # Add your solution here
-    array = array.sort 
-    puts array.first
+    new_array = array.sort 
+    puts new_array.first
 end
 
 find_min_value([10,5,3,7,19,1,3,10])
